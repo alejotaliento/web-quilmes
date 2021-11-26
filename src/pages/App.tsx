3a63a7c2c7337eee354ca247f9d10842f8192c8e
ataliento <ataliento@andreani.com>
2021-11-26 16:06:28 -0300

    feat: add new Section

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Stack, Button, Text, VStack, useBreakpointValue } from "@chakra-ui/react";
+import { Stack, Button, Text, VStack, useBreakpointValue, Box } from "@chakra-ui/react";
 import { ArrowDownIcon } from "@chakra-ui/icons";
 
 import { Banner, SplitWithImage } from "../components";
@@ -8,15 +8,15 @@ import styles from "../styles/Home.module.scss";
 function App() {
    return (
       <>
-         <section className={styles.principalSection}>
+         <Box as="section" className={styles.principalSection}>
             <Banner image="https://image.freepik.com/free-photo/glass-bottles-beer-with-glass-ice-dark-background_1150-8901.jpg">
                <VStack
-                  bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
-                  justify={"center"}
+                  bgGradient="linear(to-r, blackAlpha.600, transparent)"
+                  justify="center"
                   px={useBreakpointValue({ base: 4, md: 8 })}
-                  w={"full"}
+                  w="full"
                >
-                  <Stack align={"flex-start"} maxW={"2xl"} spacing={4}>
+                  <Stack align="flex-start" maxW="2xl" spacing={4}>
                      <Text as="h1" className={styles.title}>
                         EL SABOR DEL <br /> ENCUENTRO
                      </Text>
@@ -24,27 +24,63 @@ function App() {
                         Cerveza que te acompaña <br /> en todo momento.
                      </Text>
 
-                     <Stack alignSelf="flex-start" direction={"row"}>
-                        <Button className={styles.buttonBanner} rounded={"full"}>
+                     <Box alignSelf="flex-start" direction="row">
+                        <Button
+                           className={styles.buttonBanner}
+                           margin="5px 5px 5px 50px"
+                           rounded="full"
+                        >
                            VER MÁS
                         </Button>
-                     </Stack>
+                     </Box>
                   </Stack>
                </VStack>
 
-               <Button className={styles.rowButton} rounded={"full"}>
-                  <ArrowDownIcon h={6} w={6} />
-               </Button>
-               <Text as="h3" className={styles.endText}>
-                  Desde 1888 en las puertas de nuestras casas en Quilmes te acompañamos en todos tus
-                  encuentros. Somos la cerveza argentina con 200 años de historia.
-               </Text>
-            </Banner>
-         </section>
+               <Box alignSelf="flex-end">
+                  <Button className={styles.rowButton} rounded="full">
+                     <ArrowDownIcon h={4} w={4} />
+                  </Button>
+               </Box>
 
-         <section>
-            <SplitWithImage />
-         </section>
+               <Box className={styles.endText}>
+                  <Text as="h3">
+                     Desde 1888 en las puertas de nuestras casas en Quilmes te acompañamos en todos
+                     tus encuentros. Somos la cerveza argentina con 200 años de historia.
+                  </Text>
+               </Box>
+            </Banner>
+         </Box>
+
+         <Box as="section">
+            <SplitWithImage
+               containerProps={{
+                  bgColor: "blue",
+               }}
+               image={
+                  "https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+               }
+            >
+               <VStack
+                  align="flex-start"
+                  justify="center"
+                  maxW="2xl"
+                  px={useBreakpointValue({ base: 4, md: 8 })}
+                  spacing={4}
+                  textAlign="center"
+                  w="full"
+               >
+                  <Text as="h1" className={styles.title}>
+                     Lorem ipsum dolor sit amet
+                  </Text>
+                  <Text as="h2" className={styles.subtitle}>
+                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  </Text>
+                  <Button alignSelf="center" className={styles.buttonBanner} rounded="full">
+                     CONOCER
+                  </Button>
+               </VStack>
+            </SplitWithImage>
+         </Box>
       </>
    );
 }

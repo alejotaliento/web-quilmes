55d3e6f7b360badb66f6670ca7e9566ab59c2a26
Alejo Taliento <alee.taliento@gmail.com>
2021-11-25 21:28:51 -0300

    styles: Banner title,subtitle, endText

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Stack, Button, Text, VStack, useBreakpointValue } from "@chakra-ui/react";
+import { ArrowDownIcon } from "@chakra-ui/icons";
 
 import { Banner, SplitWithImage } from "../components";
 import styles from "../styles/Home.module.scss";
@@ -7,49 +8,43 @@ import styles from "../styles/Home.module.scss";
 function App() {
    return (
       <>
-         <Banner image="https://image.freepik.com/free-photo/glass-bottles-beer-with-glass-ice-dark-background_1150-8901.jpg">
-            <VStack
-               bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
-               justify={"center"}
-               px={useBreakpointValue({ base: 4, md: 8 })}
-               w={"full"}
-            >
-               <Stack align={"flex-start"} maxW={"2xl"} spacing={6}>
-                  <Text
-                     as="h1"
-                     color="white"
-                     fontSize={useBreakpointValue({ base: "45px", md: "60px" })}
-                     fontWeight={700}
-                     lineHeight={1.2}
-                  >
-                     EL SABOR DEL <br /> ENCUENTRO
-                  </Text>
-                  <Text as="h2" color="white" fontSize={25}>
-                     Cerveza que te acompaña <br /> en todo momento.
-                  </Text>
+         <section className={styles.principalSection}>
+            <Banner image="https://image.freepik.com/free-photo/glass-bottles-beer-with-glass-ice-dark-background_1150-8901.jpg">
+               <VStack
+                  bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+                  justify={"center"}
+                  px={useBreakpointValue({ base: 4, md: 8 })}
+                  w={"full"}
+               >
+                  <Stack align={"flex-start"} maxW={"2xl"} spacing={4}>
+                     <Text as="h1" className={styles.title}>
+                        EL SABOR DEL <br /> ENCUENTRO
+                     </Text>
+                     <Text as="h2" className={styles.subtitle}>
+                        Cerveza que te acompaña <br /> en todo momento.
+                     </Text>
 
-                  <Stack alignSelf="flex-start" direction={"row"}>
-                     <Button className={styles.buttonBanner} rounded={"full"}>
-                        VER MÁS
-                     </Button>
+                     <Stack alignSelf="flex-start" direction={"row"}>
+                        <Button className={styles.buttonBanner} rounded={"full"}>
+                           VER MÁS
+                        </Button>
+                     </Stack>
                   </Stack>
-               </Stack>
-            </VStack>
-            <Text
-               alignSelf="flex-end"
-               as="h3"
-               color="white"
-               fontSize={20}
-               margin={4}
-               textAlign="center"
-               width="70%"
-            >
-               Desde 1888 en las puertas de nuestras casas en Quilmes te acompañamos en todos tus
-               encuentros. Somos la cerveza argentina con 200 años de historia.
-            </Text>
-         </Banner>
+               </VStack>
 
-         <SplitWithImage />
+               <Button className={styles.rowButton} rounded={"full"}>
+                  <ArrowDownIcon h={6} w={6} />
+               </Button>
+               <Text as="h3" className={styles.endText}>
+                  Desde 1888 en las puertas de nuestras casas en Quilmes te acompañamos en todos tus
+                  encuentros. Somos la cerveza argentina con 200 años de historia.
+               </Text>
+            </Banner>
+         </section>
+
+         <section>
+            <SplitWithImage />
+         </section>
       </>
    );
 }

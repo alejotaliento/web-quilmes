08ae3c54dc24c08c524018fae6ca5571b9a5459b
ataliento <ataliento@andreani.com>
2021-11-26 16:06:14 -0300

    feat: fix SplitWithImage

--- src/components/SplitWithImage/index.tsx
+++ src/components/SplitWithImage/index.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-import {
-   Container,
-   SimpleGrid,
-   Image,
-   Flex,
-   Heading,
-   Text,
-   Stack,
-   StackDivider,
-   // Icon,
-   useColorModeValue,
-} from "@chakra-ui/react";
-// import { IoAnalyticsSharp, IoLogoBitcoin, IoSearchSharp } from "react-icons/io5";
+import { Container, SimpleGrid, Image, Flex, Text, Stack } from "@chakra-ui/react";
+import type { ContainerProps } from "@chakra-ui/react";
 import { ReactElement } from "react";
 
 export interface FeatureProps {
    text: string;
    iconBg: string;
    icon?: ReactElement;
+}
+
+export interface SplitWithImage {
+   containerProps?: ContainerProps;
+   image: string;
 }
 
 export const Feature = ({ text, icon, iconBg }: FeatureProps) => {
@@ -31,56 +25,13 @@ export const Feature = ({ text, icon, iconBg }: FeatureProps) => {
    );
 };
 
-export const SplitWithImage = () => {
+export const SplitWithImage: React.FC<SplitWithImage> = ({ children, image, containerProps }) => {
    return (
-      <Container maxW={"5xl"} py={12}>
-         <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-            <Stack spacing={4}>
-               <Text
-                  alignSelf={"flex-start"}
-                  fontSize={"sm"}
-                  fontWeight={600}
-                  p={2}
-                  rounded={"md"}
-                  textTransform={"uppercase"}
-               >
-                  Our Story
-               </Text>
-               <Heading>A digital Product design agency</Heading>
-               <Text color={"gray.500"} fontSize={"lg"}>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                  tempor invidunt ut labore
-               </Text>
-               <Stack
-                  divider={<StackDivider borderColor={useColorModeValue("gray.100", "gray.700")} />}
-                  spacing={4}
-               >
-                  <Feature
-                     // icon={<Icon as={IoAnalyticsSharp} color={"yellow.500"} h={5} w={5} />}
-                     iconBg={useColorModeValue("yellow.100", "yellow.900")}
-                     text={"Business Planning"}
-                  />
-                  <Feature
-                     // icon={<Icon as={IoLogoBitcoin} color={"green.500"} h={5} w={5} />}
-                     iconBg={useColorModeValue("green.100", "green.900")}
-                     text={"Financial Planning"}
-                  />
-                  <Feature
-                     // icon={<Icon as={IoSearchSharp} color={"purple.500"} h={5} w={5} />}
-                     iconBg={useColorModeValue("purple.100", "purple.900")}
-                     text={"Market Analysis"}
-                  />
-               </Stack>
-            </Stack>
-            <Flex>
-               <Image
-                  alt={"feature image"}
-                  objectFit={"cover"}
-                  rounded={"md"}
-                  src={
-                     "https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-                  }
-               />
+      <Container maxW="full" padding={0} {...containerProps}>
+         <SimpleGrid columns={{ base: 2, md: 2 }} h="100vh">
+            {children}
+            <Flex h="full">
+               <Image alt={"feature image"} objectFit={"cover"} src={image} />
             </Flex>
          </SimpleGrid>
       </Container>

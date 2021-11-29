22cfca84f90ef39d1d9807b92317edffb8034d36
ataliento <ataliento@andreani.com>
2021-11-28 23:24:21 -0300

    fix: remove Feature component

--- src/components/SplitWithImage/index.tsx
+++ src/components/SplitWithImage/index.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { Container, SimpleGrid, Image, Flex, Text, Stack } from "@chakra-ui/react";
+import { Container, SimpleGrid, Image, Flex, useMediaQuery } from "@chakra-ui/react";
 import type { ContainerProps } from "@chakra-ui/react";
-import { ReactElement } from "react";
-
-export interface FeatureProps {
-   text: string;
-   iconBg: string;
-   icon?: ReactElement;
-}
 
 export interface SplitWithImage {
    containerProps?: ContainerProps;
@@ -15,35 +8,26 @@ export interface SplitWithImage {
    imageOrientation: "left" | "right";
 }
 
-export const Feature = ({ text, icon, iconBg }: FeatureProps) => {
-   return (
-      <Stack align={"center"} direction={"row"}>
-         <Flex align={"center"} bg={iconBg} h={8} justify={"center"} rounded={"full"} w={8}>
-            {icon}
-         </Flex>
-         <Text fontWeight={600}>{text}</Text>
-      </Stack>
-   );
-};
-
 export const SplitWithImage: React.FC<SplitWithImage> = ({
    children,
    image,
    containerProps,
    imageOrientation,
 }) => {
+   const [isLargerThan768] = useMediaQuery("(max-width: 768px)");
+
    return (
       <Container maxW="full" padding={0} {...containerProps}>
-         <SimpleGrid columns={{ base: 1, md: 2 }} h="100vh">
+         <SimpleGrid columns={{ base: 1, md: 2 }} h={isLargerThan768 ? "auto" : "100vh"}>
             {imageOrientation && imageOrientation === "left" && (
                <Flex h="full" w="full">
-                  <Image alt={"feature image"} objectFit={"cover"} src={image} />
+                  <Image alt={"feature image"} objectFit={"cover"} src={image} width="100%" />
                </Flex>
             )}
             {children}
             {imageOrientation && imageOrientation === "right" && (
                <Flex h="full" w="full">
-                  <Image alt={"feature image"} objectFit={"cover"} src={image} />
+                  <Image alt={"feature image"} objectFit={"cover"} src={image} width="100%" />
                </Flex>
             )}
          </SimpleGrid>

ab052622bb765dc156d4097a4a3d04c763bb9bd0
ataliento <ataliento@andreani.com>
2021-11-28 21:52:24 -0300

    feat: add Card component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,9 @@ export { Banner } from "./Banner";
 export { Footer } from "./Footer";
 export { Feature, SplitWithImage } from "./SplitWithImage";
 export { SocialButton } from "./SocialButton";
+export { Card } from "./Card";
 
 export type { BannerProps } from "./Banner";
 export type { FeatureProps } from "./SplitWithImage";
 export type { SocialButtonProps } from "./SocialButton";
+export type { CardProps } from "./Card";

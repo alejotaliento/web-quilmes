0e5294502f344ee4821a6bd75f5812e9377524f1
ataliento <ataliento@andreani.com>
2021-11-30 18:28:16 -0300

    fix: props SplitWithImage

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,5 +7,6 @@ export { SocialButton } from "./SocialButton";
 export { Card } from "./Card";
 
 export type { BannerProps } from "./Banner";
+export type { SplitWithImageProps } from "./SplitWithImage";
 export type { SocialButtonProps } from "./SocialButton";
 export type { CardProps } from "./Card";
